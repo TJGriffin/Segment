3584d2943aab51042a7c1ac03bd058cb9fbf7c84
TJ <tj@tjgriffin.com>
2023-08-22 14:39:20 -0500

    make the display clearer

--- force-app/main/default/lwc/accStatusWidget/accStatusWidget.js
+++ force-app/main/default/lwc/accStatusWidget/accStatusWidget.js
@@ -1,12 +1,16 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AccStatusWidget extends LightningElement {
+    @api statusField;
     @api statusLabel;
     @api statusDate;
     @api downgradeByName;
     @api downgradeById;
     @api downgradeDate;
 
+    get displayLabel(){
+        return this.statusField+': '+this.statusLabel;
+    }
     get downgradeLink(){
         return '/'+this.downgradeById;
     }
